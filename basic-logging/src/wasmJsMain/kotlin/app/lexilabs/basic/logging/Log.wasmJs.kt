5834fca17e3c9d644baec1800134416b06682a0a
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-04-25 21:13:09 -0400

    V0.2.6 beta03 (#9)
    * Bump com.android.library from 8.9.1 to 8.9.2
    
    Bumps com.android.library from 8.9.1 to 8.9.2.
    
    ---
    updated-dependencies:
    - dependency-name: com.android.library
      dependency-version: 8.9.2
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * COMPLETELY CHANGED WASM CODE
    kotlin = 2.1.21-RC
    AGP = 8.9.2
    Logging version shifted to TOML catalogue
    Upgraded Dokka formats
    Updated image from PNG to SVG
    Removed a bunch of lame rules in the publish.yaml workflow
    Improved README.md
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- basic-logging/src/wasmJsMain/kotlin/app/lexilabs/basic/logging/Log.wasmJs.kt
+++ basic-logging/src/wasmJsMain/kotlin/app/lexilabs/basic/logging/Log.wasmJs.kt
@@ -2,25 +2,25 @@ package app.lexilabs.basic.logging
 
 public actual object Log : Logger {
     actual override fun v(tag: String, message: String) {
-        println("VERBOSE: [$tag] $message")
+        js("console.debug('[$tag] $message')")
     }
 
     actual override fun d(tag: String, message: String) {
-        println("DEBUG: [$tag] $message")
+        js("console.debug('[$tag] $message')")
     }
 
     actual override fun i(tag: String, message: String) {
-        println("INFO: [$tag] $message")
+        js("console.log('[$tag] $message')")
     }
 
     actual override fun w(tag: String, message: String) {
-        println("WARN: [$tag] $message")
+        js("console.warn('[$tag] $message')")
     }
 
     actual override fun e(tag: String, message: String) {
-        println("ERROR: [$tag] $message")
+        js("console.error('[$tag] $message')")
     }
     actual override fun wtf(tag: String, message: String) {
-        println("WHAT-THE-F***: [$tag] $message")
+        js("console.error('WHAT-THE-F***: [$tag] $message')")
     }
 }
