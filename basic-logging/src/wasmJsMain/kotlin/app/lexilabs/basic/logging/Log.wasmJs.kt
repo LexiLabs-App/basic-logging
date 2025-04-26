a89be29987bf5825e00a2cdf7f83e8419825a2e9
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-04-25 21:38:32 -0400

    HOTFIX - Wasm logger redesigned again. Updated API dump

--- basic-logging/src/wasmJsMain/kotlin/app/lexilabs/basic/logging/Log.wasmJs.kt
+++ basic-logging/src/wasmJsMain/kotlin/app/lexilabs/basic/logging/Log.wasmJs.kt
@@ -1,26 +1,15 @@
 package app.lexilabs.basic.logging
 
 public actual object Log : Logger {
-    actual override fun v(tag: String, message: String) {
-        js("console.debug('[$tag] $message')")
-    }
+    actual override fun v(tag: String, message: String): Unit = debug(tag, message)
 
-    actual override fun d(tag: String, message: String) {
-        js("console.debug('[$tag] $message')")
-    }
+    actual override fun d(tag: String, message: String): Unit = debug(tag, message)
 
-    actual override fun i(tag: String, message: String) {
-        js("console.log('[$tag] $message')")
-    }
+    actual override fun i(tag: String, message: String): Unit = info(tag, message)
 
-    actual override fun w(tag: String, message: String) {
-        js("console.warn('[$tag] $message')")
-    }
+    actual override fun w(tag: String, message: String): Unit = warn(tag, message)
 
-    actual override fun e(tag: String, message: String) {
-        js("console.error('[$tag] $message')")
-    }
-    actual override fun wtf(tag: String, message: String) {
-        js("console.error('WHAT-THE-F***: [$tag] $message')")
-    }
+    actual override fun e(tag: String, message: String): Unit = error(tag, message)
+
+    actual override fun wtf(tag: String, message: String): Unit = whatTheF(tag, message)
 }
