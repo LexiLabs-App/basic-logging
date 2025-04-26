82778ad54d69b941b77b628fec5eb13607599f99
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-04-25 21:39:39 -0400

    V0.2.6 beta03 (#10)
    * Bump com.android.library from 8.9.1 to 8.9.2
    
    Bumps com.android.library from 8.9.1 to 8.9.2.
    
    ---
    updated-dependencies:
    - dependency-name: com.android.library
      dependency-version: 8.9.2
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * COMPLETELY CHANGED WASM CODE
    kotlin = 2.1.21-RC
    AGP = 8.9.2
    Logging version shifted to TOML catalogue
    Upgraded Dokka formats
    Updated image from PNG to SVG
    Removed a bunch of lame rules in the publish.yaml workflow
    Improved README.md
    
    * HOTFIX - Wasm logger redesigned again.
    Updated API dump
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ basic-logging/src/wasmJsMain/kotlin/app/lexilabs/basic/logging/Connectors.kt
@@ -0,0 +1,9 @@
+@file:JsModule("logger.js")
+
+package app.lexilabs.basic.logging
+
+public external fun debug(tag: String, message: String)
+public external fun info(tag: String, message: String)
+public external fun warn(tag: String, message: String)
+public external fun error(tag: String, message: String)
+public external fun whatTheF(tag: String, message: String)
---
+++ basic-logging/src/wasmJsMain/resources/logger.js
@@ -0,0 +1,19 @@
+function debug (tag, message) {
+    console.debug("[" + tag + "] " + message);
+}
+
+function info (tag, message) {
+    console.log("[" + tag + "] " + message);
+}
+
+function warn (tag, message) {
+    console.warn("[" + tag + "] " + message);
+}
+
+function error (tag, message) {
+    console.error("[" + tag + "] " + message);
+}
+
+function whatTheF (tag, message) {
+    console.error("WHAT-THE-F***: [" + tag + "] " + message);
+}
