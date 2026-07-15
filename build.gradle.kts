5a3fefbb4599dd919bf0cb0331a5229e419cebd4
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2026-07-15 17:14:54 -0400

    V0.3.0 (#36)
    * Init and Update
    
    * Update publishing workflow and dependabot configuration
    
    - Added "publishing" target branch to Dependabot config.
    - Updated Xcode version setup for compatibility.
    - Revised MavenCentral publishing step for improved handling of secrets and Gradle properties.
    - Adjusted documentation jobs to run on macOS and updated Dokka task name.
    
    * Bump AGP to 9.3.0 and clean up `gradle.properties`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,18 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import org.jetbrains.dokka.base.DokkaBase
-import org.jetbrains.dokka.base.DokkaBaseConfiguration
-import org.jetbrains.dokka.gradle.DokkaTask
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+
 
 plugins {
     alias(libs.plugins.multiplatform).apply(false)
-    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.multiplatform.library).apply(false)
     alias(libs.plugins.dokka)
     alias(libs.plugins.kover)
 }
 
-dependencies {
-    kover(project(":basic-logging"))
-}
+dependencies { kover(project(":basic-logging")) }
 
 buildscript {
-    dependencies {
-        classpath(libs.dokka.base)
-    }
+    plugins { alias(libs.plugins.maven.publish)}
+    dependencies { classpath(libs.dokka.base) }
 }
 
 allprojects {
@@ -25,103 +20,59 @@ allprojects {
     version = rootProject.libs.versions.logging.get()
 
     apply(plugin = "org.jetbrains.dokka")
-    apply(plugin = "maven-publish")
-    apply(plugin = "signing")
+    apply(plugin = "com.vanniktech.maven.publish")
 
-    extensions.configure<PublishingExtension> {
-        repositories {
-            maven {
-                name = "maven"
-                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
-                credentials {
-                    username = gradleLocalProperties(rootDir, providers).getProperty("sonatypeUsername")
-                    password = gradleLocalProperties(rootDir, providers).getProperty("sonatypePassword")
-                }
-            }
+    /** dokka generation **/
+    dokka {
+        moduleName.set(project.name)
+        moduleVersion.set(project.version.toString())
+        dokkaPublications.html {
+            outputDirectory.set(rootDir.resolve("docs"))
+            suppressObviousFunctions.set(true)
+            suppressInheritedMembers.set(false)
+            failOnWarning.set(false)
+            offlineMode.set(false)
         }
-
-        val javadocJar = tasks.register<Jar>("javadocJar") {
-            dependsOn(tasks.dokkaHtml)
-            archiveClassifier.set("javadoc")
-            from("${layout.buildDirectory}/dokka")
+        pluginsConfiguration.html {
+            customAssets.from(rootDir.resolve("images/logo-icon.svg"))
+            footerMessage.set("(c) 2026 LexiLabs")
         }
+    }
 
-        /** dokka generation **/
-        tasks.register<Delete>("clearDokkaHtml") {
-            delete("${projectDir.parent}/docs")
-        }
-        tasks.withType<DokkaTask>().configureEach{
-            pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
-                dependsOn("clearDokkaHtml")
-                outputDirectory = file("${projectDir.parent}/docs")
-                moduleName = project.name
-                moduleVersion = project.version.toString()
-                customAssets = listOf(file("${projectDir.parent}/images/logo-icon.svg"))
-                // Need to create a cool looking theme at some point
-                //customStyleSheets = listOf(file("${projectDir.parent}/dokka/styles.css"))
-                footerMessage = "(c) 2025 LexiLabs"
-                failOnWarning = false
-                suppressObviousFunctions = true
-                suppressInheritedMembers = false
-                offlineMode = false
-            }
-        }
+    extensions.configure<MavenPublishBaseExtension> {
 
-        publications {
-            withType<MavenPublication> {
-                artifact(javadocJar)
+        mavenPublishing {
+            publishToMavenCentral(automaticRelease = true)
 
-                pom {
-                    name.set("Basic")
-                    description.set("Easily integrate logging into your Kotlin Multiplatform Mobile (KMP / KMM) project")
-                    licenses {
-                        license {
-                            name.set("MIT License")
-                            url.set("https://raw.githubusercontent.com/LexiLabs-App/basic-logging/refs/heads/main/LICENSE")
-                        }
+            signAllPublications()
+            coordinates(group.toString(), project.name, version.toString())
+            pom {
+                name.set("Basic")
+                description.set("Easily integrate logging into your Kotlin Multiplatform Mobile (KMP / KMM) project")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://raw.githubusercontent.com/LexiLabs-App/basic-logging/refs/heads/main/LICENSE")
                     }
+                }
+                url.set("https://github.com/LexiLabs-App/basic-logging")
+                issueManagement {
+                    system.set("Github")
+                    url.set("https://github.com/LexiLabs-App/basic-logging/issues")
+                }
+                scm {
+                    connection.set("https://github.com/LexiLabs-App/basic-logging.git")
                     url.set("https://github.com/LexiLabs-App/basic-logging")
-                    issueManagement {
-                        system.set("Github")
-                        url.set("https://github.com/LexiLabs-App/basic-logging/issues")
-                    }
-                    scm {
-                        connection.set("https://github.com/LexiLabs-App/basic-logging.git")
-                        url.set("https://github.com/LexiLabs-App/basic-logging")
-                    }
-                    developers {
-                        developer {
-                            id.set("rjamison")
-                            name.set("Robert Jamison")
-                            email.set("rjamison@lexilabs.app")
-                            url.set("https://logging.basic.lexilabs.app")
-                        }
+                }
+                developers {
+                    developer {
+                        id.set("rjamison")
+                        name.set("Robert Jamison")
+                        email.set("rjamison@lexilabs.app")
+                        url.set("https://logging.basic.lexilabs.app")
                     }
                 }
             }
         }
-    }
-
-    val publishing = extensions.getByType<PublishingExtension>()
-
-    if (gradle.startParameter.taskNames.any { it == "publish" }) {
-        extensions.configure<SigningExtension> {
-            useInMemoryPgpKeys(
-                gradleLocalProperties(rootDir, providers).getProperty("gpgKeyId"),
-                gradleLocalProperties(rootDir, providers).getProperty("gpgKeySecret"),
-                gradleLocalProperties(rootDir, providers).getProperty("gpgKeyPassword")
-            )
-            sign(publishing.publications)
-        }
-    } else {
-        extensions.configure<SigningExtension> {
-            useGpgCmd()
-            sign(publishing.publications)
-        }
-    }
-
-    // remove after https://youtrack.jetbrains.com/issue/KT-46466 is fixed
-    project.tasks.withType(AbstractPublishToMaven::class.java).configureEach {
-        dependsOn(project.tasks.withType(Sign::class.java))
     }
 }
